e4b331c38e280da309bc2faba344d922e77eb942
Hansmartin Geiser <semiroot@users.noreply.github.com>
2025-04-23 22:46:42 +0200

    Implemented sender.

---
+++ Sender/Sender.ino
@@ -0,0 +1,82 @@
+#include <RCSwitch.h>
+
+#include "Config.h"
+#include "RotaryTrigger.h"
+#include "RadioSignal.h"
+
+// #define DEBUG
+
+#ifdef DEBUG
+RotaryTrigger rotaryTrigger;
+bool dipOverrideOn = true;
+#endif
+
+// === RF Sender ===
+RCSwitch sender = RCSwitch();
+
+// === Internal State ===
+bool isOn = false;
+
+void setup() {
+  
+  #ifdef DEBUG
+    Serial.begin(9600);
+  #endif
+
+  randomSeed(analogRead(A0)); // Seed for RF signal randomness
+
+  pinMode(Config::State::LedPin, OUTPUT);
+  pinMode(Config::State::PowerProbePin, INPUT);
+
+  sender.enableTransmit(Config::Radio::SenderPin);
+
+#ifdef DEBUG
+  // Rotary input simulates DIP switch override in debug mode
+  rotaryTrigger.Setup();
+  attachInterrupt(
+    digitalPinToInterrupt(Config::Rotary::ClockPin),
+    []() { rotaryTrigger.HandleInterrupt(); },
+    RISING
+  );
+#endif
+}
+
+void loop() {
+  // === Check power state ===
+  // If power is present → send ON signal
+  // If power is lost → send OFF signal
+  bool isPowered = digitalRead(Config::State::PowerProbePin);
+
+#ifdef DEBUG
+  // Override actual power state using rotary in debug mode
+  isPowered = dipOverrideOn;
+
+  if (rotaryTrigger.HasChanged()) {
+    dipOverrideOn = !dipOverrideOn;
+    isPowered = dipOverrideOn;
+  }
+#endif
+
+  // Avoid duplicate signals if state hasn't changed
+  if (isPowered == isOn) return;
+
+  if (isPowered) {
+    SendOn();
+  } else {
+    SendOff();
+  }
+}
+
+// === Send ON Signal ===
+void SendOn() {
+  isOn = true;
+  digitalWrite(Config::State::LedPin, HIGH);
+  RadioSignal::Create(true).Send(&sender);
+}
+
+// === Send OFF Signal ===
+void SendOff() {
+  isOn = false;
+  digitalWrite(Config::State::LedPin, LOW);
+  RadioSignal::Create(false).Send(&sender);
+}
